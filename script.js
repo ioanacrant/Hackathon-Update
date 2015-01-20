94b7b2766fd6c7af6e5def0af4a641feaf4a337b
Ioana Crant <ioana.crant@gmail.com>
2015-01-19 20:36:29 -0500

    one

---
+++ script.js
@@ -0,0 +1,5 @@
+function processForm(){
+    var emaill=document.subscribeform.emailinput.value;
+                 //  alert(emaill);
+	document.getElementById("subbed").innerHTML+='<div>Subscribed!</div';
+}
