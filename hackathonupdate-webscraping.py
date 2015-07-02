badd25a508cc68a68ac821a2f5ec99b8b747aab3
Ioana Crant <ioana.crant@gmail.com>
2015-07-01 19:41:59 -0700

    added emailing, differentiating new&old

--- hackathonupdate-webscraping.py
+++ hackathonupdate-webscraping.py
@@ -1,12 +1,37 @@
 from lxml import html
 import requests
+import sendgrid
 
+sguser="djsgkjg"
+sgpass="fdggfd"
+inf= open("output.txt")
+oldhackathons=inf.readlines()
+inf.close()
 
-page=requests.get('https://mlh.io/seasons/s2015/events.html')
+outf=open("output.txt","w")
+
+reqpage='https://mlh.io/seasons/f2015/events.html'
+page=requests.get(reqpage)
 tree=html.fromstring(page.text)
 
-
-titles=tree.xpath('//*[name()="h3"]/text()')
+def get_hackathon_names():
+    titles=tree.xpath('//*[name()="h3"]/text()')
+    return titles
 #add dates and locations later
 
-print(titles)
+newhackathons=get_hackathon_names()
+
+if len(oldhackathons)!=len(newhackathons):
+    addedhackathons=list(set(newhackathons)-set(oldhackathons))
+
+    sg = sendgrid.SendGridClient(sguser, sgpass)
+    message = sendgrid.Mail()
+    message.add_to("ioana.crant@gmail.com")
+    message.set_from("hackathon.update@gmail.com")
+    message.set_subject("Some hackathons have been added to MLH!")
+    message.set_html("Hey, check out MLH.io!")
+
+    sg.send(message)
+
+outf.write ("\n".join(newhackathons))
+outf.close()
